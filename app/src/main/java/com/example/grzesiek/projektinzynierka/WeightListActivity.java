2b59ffc9da4b28e0cace05ac2426799d82e15de4
adamkubarek <adamkubarek@gmail.com>
2018-04-19 21:10:18 +0200

    Proper display of a list with measurements

--- app/src/main/java/com/example/grzesiek/projektinzynierka/WeightListActivity.java
+++ app/src/main/java/com/example/grzesiek/projektinzynierka/WeightListActivity.java
@@ -1,28 +1,38 @@
 package com.example.grzesiek.projektinzynierka;
 
-import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toolbar;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class WeightListActivity extends AppCompatActivity {
 
-    ArrayList<DataModelWeight> dataModelWeights;
-    ListView listViewl;
-    private static CustomAdapterWeight WeightAdapter;
+    private ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weight_list);
-
-/*
         DatabaseHandler db = new DatabaseHandler(this);
+        list = findViewById(R.id.weight_list);
 
+        prepareData(db);
+        List<String> dane = new ArrayList<>();
+
+        List<weight> weights = db.getAllWeight();
+
+        for (weight wg : weights) {
+            String record = wg.getYear() + "." + wg.getMonth() + "." + wg.getDay() + " waga: " + wg.getWeight() + "\n";
+            dane.add(record);
+        }
+
+        list.setAdapter(new ArrayAdapter<>(this, R.layout.list_item, R.id.row, dane));
+    }
+
+    private void prepareData(DatabaseHandler db) {
         db.addInformation(new information("Grzesiek", 184, 120.5, 100.5, 25));
         db.addWeight(new weight(2018, 3, 8, 119.5));
         db.addWeight(new weight(2018, 4, 10, 129.5));
@@ -31,21 +41,5 @@ public class WeightListActivity extends AppCompatActivity {
         db.addCircuit(new circuit(2018, 4, 10, 116.0, 117.5));
         db.addCircuit(new circuit(2018, 5, 8, 110.0, 115.6));
         db.addCircuit(new circuit(2018, 6, 12, 112.0, 114.7));
-
-        List<String> dane = new ArrayList<String>();
-
-        List<weight> weights = db.getAllWeight();
-        for (weight wg : weights) {
-            String log = wg.getYear() + "." + wg.getMonth() + "." + wg.getDay() + " waga: " + wg.getWeight() + "\n";
-            dane.add(log);
-            // Writing Contacts to log
-            Log.d("dane: ", String.valueOf(dane));
-            Log.i("dane: ", String.valueOf(dane));
-            //   Log.v("dane: ", log);
-        }
-        this.setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, R.id.row, dane));
-
-*/
-
     }
 }
